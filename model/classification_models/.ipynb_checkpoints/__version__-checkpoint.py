71f5381f32f8d2aee239f8001efe79a3a6e00d57
lixg <lixg95@126.com>
2021-03-30 14:22:02 +0800

    整理

--- model/classification_models/.ipynb_checkpoints/__version__-checkpoint.py
+++
@@ -1,3 +0,0 @@
-VERSION = (0, 1, '0rc0')
-
-__version__ = '.'.join(map(str, VERSION))
--- model/segmentation_models/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,6 +0,0 @@
-name = "segmentation_models"
-
-from .unet import Unet
-from .fpn import FPN
-from .linknet import Linknet
-from .pspnet import PSPNet
